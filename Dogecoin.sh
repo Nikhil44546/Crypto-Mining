01e27a5a9b5e2a045a37722a13688f671cbf9d2c
Nikhil44546 <108381522+Nikhil44546@users.noreply.github.com>
2022-07-16 06:03:14 +0530

    Create Dogecoin.sh

---
+++ Dogecoin.sh
@@ -0,0 +1,6 @@
+
+sudo apt update
+! wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz
+! tar -xf lolMiner_v1.28a_Lin64.tar.gz
+%cd 1.28a/
+!./lolMiner --algo ETHASH --pool etchash.unmineable.com:3333 --user DOGE:DBrHbdJ2K6KLoN9gf9SnYvR2KQ7JUtmHv6.github --ethstratum ETHPROXY
